79f00600117b972263eeedae1cab179f93f583c5
kevin <kevingu980@gmail.com>
2026-05-06 00:19:58 +1000

    Customise docker files for canbus test

--- docker/devel/docker-build.sh
+++ docker/devel/docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-docker build --pull --no-cache --tag pika_spark_docker_devel .
+docker build --pull --no-cache --tag pika_spark_docker_devel_canbus_test .
